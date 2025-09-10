d43ba1617d5ea3b5f87dbba9f4dc9deaf3f9cc63
Carinan02 <141795962+Carinan02@users.noreply.github.com>
2025-09-10 02:40:56 +0000

    need to move files

---
+++ react-project/src/App.jsx
@@ -0,0 +1,43 @@
+import "./App.css";
+
+function Header({ name, year }) {
+  return (
+    <header>
+      <h1>{name}'s Kitchen</h1>
+      <p>Copyright {year}</p>
+    </header>
+  );
+}
+
+const items = [
+  "Macaroni and Cheese",
+  "Salmon with Potatoes",
+  "Tofu with Vegetables",
+  "Minestrone Soup",
+];
+
+const dishObjects = items.map((dish, i) => ({
+  id: i,
+  title: dish,
+}));
+function Main({ dishes }) {
+  return (
+    <ul>
+      {dishes.map((dish) => (
+        <li key={dish.id} style={{ listStyleType: "none" }}>
+          {dish.title}
+        </li>
+      ))}
+    </ul>
+  );
+}
+const App = () => {
+  return (
+    <div>
+      <Header name="Nico" year={new Date().getFullYear()} />
+      <Main dishes={dishObjects} />
+    </div>
+  );
+};
+
+export default App;
