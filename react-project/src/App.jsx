bc5586d5de74bf4793508a1741921690c50d8b9d
Carinan02 <141795962+Carinan02@users.noreply.github.com>
2025-09-10 05:23:57 +0000

    main before creating backend react server components

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,4 +1,6 @@
+import { useEffect, useState, useReducer } from "react";
 import "./App.css";
+import chef from "./images/chef.jpg";
 
 function Header({ name, year }) {
   return (
@@ -20,22 +22,41 @@ const dishObjects = items.map((dish, i) => ({
   id: i,
   title: dish,
 }));
-function Main({ dishes }) {
+function Main({ dishes, openStatus, onStatus }) {
   return (
-    <ul>
-      {dishes.map((dish) => (
-        <li key={dish.id} style={{ listStyleType: "none" }}>
-          {dish.title}
-        </li>
-      ))}
-    </ul>
+    <>
+      <div>
+        <button onClick={() => onStatus(true)}>I want to be open </button>
+        <h2>
+          Welcome to this beautiful restaurant! {openStatus ? "Open" : "Close"}
+        </h2>
+      </div>
+      <main>
+        <img src={chef} height={200} alt="A photo of a smiling chef owner" />
+        <ul>
+          {dishes.map((dish) => (
+            <li key={dish.id} style={{ listStyleType: "none" }}>
+              {dish.title}
+            </li>
+          ))}
+        </ul>
+      </main>
+    </>
   );
 }
 const App = () => {
+  //const [status, setStatus] = useState(true);
+  const [status, toggle] = useReducer((status) => !status, true);
+
+  useEffect(() => {
+    console.log(`The restaurant is ${status ? "open" : "closed"}`);
+  }, [status]);
   return (
     <div>
+      <h1>The restaurant is currently {status ? "open" : "closed"}.</h1>
+      <button onClick={toggle}>{status ? "Close" : "Open"} Restaurant</button>
       <Header name="Nico" year={new Date().getFullYear()} />
-      <Main dishes={dishObjects} />
+      <Main dishes={dishObjects} openStatus={status} onStatus={toggle} />
     </div>
   );
 };
